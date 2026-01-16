677bf7fbc89b7608a54b8d3fa220a0ae26ad3755
oh413 <oh413@exeter.ac.uk>
2026-01-16 12:06:12 +0000

    updated game_functions.py

--- game_functions.py
+++ game_functions.py
@@ -7,8 +7,24 @@ def pick_value(poss_values):
 
 # function to be used in game_2: Higher or Lower
 def check_higher_lower(current_val, next_val, user_input):
-    pass
+    if user_input == "h" and (current_val < next_val):
+        return True
+    elif user_input == "l" and (current_val > next_val):
+        return True
+    else:
+        return False
 
 # function to be used in game_3: Hangman
 def process_guess(letter, board, word):
-    pass
+    word_list = list(word)
+    in_word = False
+    for i in range(len(word_list)):
+        if letter == word_list[i]:
+            board[i] = letter
+            in_word = True
+    if in_word:
+        print(f"Well done {letter} is in the word")
+        return True
+    else:
+        print(f"Sorry, {letter} is not in the word")
+        return False
